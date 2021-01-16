554a00ad460b0aad4ebfc2d32ebcdeb285944214
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-16 17:48:55 -0500

    add javadoc

--- src/main/java/dev/kscott/crash/command/CrashCommand.java
+++ src/main/java/dev/kscott/crash/command/CrashCommand.java
@@ -17,6 +17,9 @@ public class CrashCommand {
      */
     private final @NonNull PaperCommandManager<CommandSender> commandManager;
 
+    /**
+     * CrashProvider reference.
+     */
     private final @NonNull CrashProvider crashProvider;
 
     /**
