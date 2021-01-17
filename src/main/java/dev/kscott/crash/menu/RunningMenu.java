e32365f16a454102781f88165421b8295fad5281
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-16 21:18:33 -0500

    add functionality to RunningMenu

--- src/main/java/dev/kscott/crash/menu/RunningMenu.java
+++ src/main/java/dev/kscott/crash/menu/RunningMenu.java
@@ -1,8 +1,13 @@
 package dev.kscott.crash.menu;
 
+import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
 import com.github.stefvanschie.inventoryframework.pane.StaticPane;
+import dev.kscott.crash.game.GameManager;
 import dev.kscott.crash.utils.ItemBuilder;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -14,14 +19,24 @@ public class RunningMenu extends GameMenu {
      *
      * @param player Player who's intended to view this inventory
      */
-    public RunningMenu(final @NonNull Player player) {
+    public RunningMenu(final @NonNull Player player, final @NonNull GameManager gameManager) {
         super(6, "Crash: Running");
 
-        final @NonNull StaticPane pane = new StaticPane(0, 0, 9, 6);
+        final @NonNull StaticPane bgPane = new StaticPane(0, 0, 9, 6);
 
-        pane.fillWith(new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).build());
+        bgPane.fillWith(new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).build());
 
-        addPane(pane);
+
+        final @NonNull StaticPane fgPane = new StaticPane(0, 0, 9, 6);
+
+        fgPane.addItem(new GuiItem(
+                new ItemBuilder(Material.YELLOW_STAINED_GLASS_PANE)
+                        .name(Component.text(gameManager.getCurrentMultiplier()).color(NamedTextColor.YELLOW).decorate(TextDecoration.BOLD))
+                        .build()
+        ), 5, 2);
+
+        addPane(fgPane);
+        addPane(bgPane);
     }
 
 
