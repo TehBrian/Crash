e9bfdfb6bb57da79a91c60da9886a5d555f2df8c
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-16 19:33:45 -0500

    add some gui stuff to menu

--- src/main/java/dev/kscott/crash/menu/MenuManager.java
+++ src/main/java/dev/kscott/crash/menu/MenuManager.java
@@ -1,9 +1,15 @@
 package dev.kscott.crash.menu;
 
 import cloud.commandframework.paper.PaperCommandManager;
+import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
+import com.github.stefvanschie.inventoryframework.gui.type.util.Gui;
+import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import com.google.inject.Inject;
 import dev.kscott.crash.game.GameManager;
+import dev.kscott.crash.utils.ItemBuilder;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
@@ -44,8 +50,33 @@ public class MenuManager {
         this.gameManager = gameManager;
     }
 
-    private void createGameMenu() {
+    private Gui createGameMenu(final @NonNull Player player) {
+        final GameManager.GameState gameState = this.gameManager.getGameState();
 
+        if (gameState == GameManager.GameState.NOT_RUNNING) {
+            return createNotRunningGui(player);
+        }
+
+        return null;
+    }
+
+    /**
+     * Creates the {@link Gui} for {@link dev.kscott.crash.game.GameManager.GameState#NOT_RUNNING}.
+     * @return the {@link Gui}.
+     */
+    private Gui createNotRunningGui(final @NonNull Player player) {
+        final @NonNull ChestGui chestGui = new ChestGui(6, "Crash: not running");
+
+        final @NonNull StaticPane pane = new StaticPane(0, 0, 9, 6);
+        pane.fillWith(
+                new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE)
+                        .name("")
+                        .build()
+        );
+
+        chestGui.addPane(pane);
+
+        return chestGui;
     }
 
 }
