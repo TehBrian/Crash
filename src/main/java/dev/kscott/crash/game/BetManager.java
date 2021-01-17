ece8857c4348b64e6a685ded5bc0bc300fac7cdc
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-16 23:48:49 -0500

    add functionality to BetManager

--- src/main/java/dev/kscott/crash/game/BetManager.java
+++ src/main/java/dev/kscott/crash/game/BetManager.java
@@ -1,4 +1,63 @@
 package dev.kscott.crash.game;
 
+import org.bukkit.entity.Player;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Manages interactions for player bets.
+ */
 public class BetManager {
+
+    /**
+     * The Map which stores the player bets.
+     */
+    private final @NonNull Map<UUID, Long> betMap;
+
+    /**
+     * Constructs BetManager.
+     */
+    public BetManager() {
+        this.betMap = new HashMap<>();
+    }
+
+    /**
+     * Places a bet for a player.
+     * @param player Player to place bet for.
+     * @param bet bet amount.
+     */
+    public void placeBet(final @NonNull Player player, final long bet) {
+        this.betMap.put(player.getUniqueId(), bet);
+    }
+
+    /**
+     * Gets a bet for a player.
+     * @param player Player to get bet for.
+     * @return the amount the player bet.
+     */
+    public long getBet(final @NonNull Player player) {
+        return this.betMap.getOrDefault(player.getUniqueId(), 0L);
+    }
+
+    /**
+     * Removes a bet for a player
+     * @param player player to remove bet for
+     */
+    public void removeBet(final @NonNull Player player) {
+        this.betMap.remove(player.getUniqueId());
+    }
+
+    /**
+     * Removes all placed bets from the map.
+     */
+    public void reset() {
+        betMap.clear();
+    }
+
+
+
 }
