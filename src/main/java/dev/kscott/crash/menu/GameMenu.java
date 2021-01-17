ac371f0f96c3d408d18a88db7767fbf2b80a9a5b
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-16 23:32:27 -0500

    add javadocs

--- src/main/java/dev/kscott/crash/menu/GameMenu.java
+++ src/main/java/dev/kscott/crash/menu/GameMenu.java
@@ -3,6 +3,9 @@ package dev.kscott.crash.menu;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+/**
+ * The base menu class for all crash game menus.
+ */
 public abstract class GameMenu extends ChestGui {
     /**
      * Constructs a new chest GUI
