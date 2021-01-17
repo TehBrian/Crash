a34d80304b623eee2ab9d2d7d66cf00f0c31765b
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-16 23:37:14 -0500

    remove MenuModule and move provider to GameModule

--- src/main/java/dev/kscott/crash/inject/GameModule.java
+++ src/main/java/dev/kscott/crash/inject/GameModule.java
@@ -6,18 +6,31 @@ import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import dev.kscott.crash.game.CrashProvider;
 import dev.kscott.crash.game.GameManager;
+import dev.kscott.crash.menu.MenuManager;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+/**
+ * Provides objects related to the crash game.
+ */
 public class GameModule extends AbstractModule {
 
+    /**
+     * @return the {@link CrashProvider}.
+     */
     @Provides
     @Singleton
     public @NonNull CrashProvider provideCrashProvider() {
         return new CrashProvider();
     }
 
+    /**
+     * @param plugin JavaPlugin reference.
+     * @param crashProvider CrashProvider reference ({@link this#provideCrashProvider()}.
+     * @param commandManager PaperCommandManager reference.
+     * @return the {@link GameManager}.
+     */
     @Provides
     @Singleton
     public @NonNull GameManager provideGameManager(
@@ -27,5 +40,16 @@ public class GameModule extends AbstractModule {
     ) {
         return new GameManager(plugin, crashProvider, commandManager);
     }
+
+    /**
+     * @param gameManager GameManager reference.
+     * @return {@link MenuManager}.
+     */
+    @Provides
+    @Singleton
+    public @NonNull MenuManager provideMenuManager(final @NonNull GameManager gameManager) {
+        return gameManager.getMenuManager();
+    }
+
 
 }
