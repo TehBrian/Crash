f5e0b4132affbdbe2f8180887e054f7950e35609
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-16 21:19:04 -0500

    add menu updating

--- src/main/java/dev/kscott/crash/menu/MenuManager.java
+++ src/main/java/dev/kscott/crash/menu/MenuManager.java
@@ -2,17 +2,12 @@ package dev.kscott.crash.menu;
 
 import cloud.commandframework.paper.PaperCommandManager;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
-import com.github.stefvanschie.inventoryframework.gui.type.util.Gui;
-import com.github.stefvanschie.inventoryframework.pane.StaticPane;
-import com.google.inject.Inject;
 import dev.kscott.crash.game.GameManager;
-import dev.kscott.crash.utils.ItemBuilder;
-import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
@@ -42,7 +37,7 @@ public class MenuManager {
     /**
      * A List of GameMenu that holds all opened menus.
      */
-    private final @NonNull List<GameMenu> openInventories;
+    private final @NonNull List<InventoryHolder> openInventories;
 
     /**
      * Constructs MenuManager.
@@ -62,23 +57,30 @@ public class MenuManager {
     }
 
     public void updateMenus() {
-        for (final @NonNull GameMenu gameMenu : openInventories) {
-            final @NonNull HumanEntity entity = gameMenu.getViewers().get(0);
+        for (final @NonNull InventoryHolder holder : openInventories) {
+            final @NonNull HumanEntity entity = holder.getInventory().getViewers().get(0);
             if (entity instanceof Player) {
+                this.plugin.getServer().broadcast(((Player) entity).getDisplayName(), "");
                 showGameMenu((Player) entity);
             }
         }
     }
 
     public void showGameMenu(final @NonNull Player player) {
-        this.createGameMenu(player).show(player);
+        final @NonNull ChestGui menu = this.createGameMenu(player);
+
+        this.openInventories.add(menu);
+
+        menu.show(player);
     }
 
-    private Gui createGameMenu(final @NonNull Player player) {
+    private ChestGui createGameMenu(final @NonNull Player player) {
         final GameManager.GameState gameState = this.gameManager.getGameState();
 
         if (gameState == GameManager.GameState.PRE_GAME) {
             return new PreGameMenu(player, this.gameManager);
+        } else if (gameState == GameManager.GameState.RUNNING) {
+            return new RunningMenu(player, this.gameManager);
         }
 
         return new NotRunningMenu(player);
