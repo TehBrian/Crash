2e1dde788b78dacfd87fd11a20a4ca7046008922
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2021-01-16 20:31:51 -0500

    add functionality to PreGameMenu

--- src/main/java/dev/kscott/crash/menu/PreGameMenu.java
+++ src/main/java/dev/kscott/crash/menu/PreGameMenu.java
@@ -2,6 +2,7 @@ package dev.kscott.crash.menu;
 
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
 import com.github.stefvanschie.inventoryframework.pane.StaticPane;
+import dev.kscott.crash.game.GameManager;
 import dev.kscott.crash.utils.ItemBuilder;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -14,8 +15,11 @@ public class PreGameMenu extends GameMenu {
      *
      * @param player Player who's intended to view this inventory
      */
-    public PreGameMenu(final @NonNull Player player) {
-        super(6, "Crash: Pre Game");
+    public PreGameMenu(
+            final @NonNull Player player,
+            final @NonNull GameManager gameManager
+    ) {
+        super(6, "Crash: Pre Game "+gameManager.getPreGameCountdown());
 
         final @NonNull StaticPane pane = new StaticPane(0, 0, 9, 6);
 
